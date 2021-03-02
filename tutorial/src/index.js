3be31fd2cfcc2a31bc7e057c3ee1ba1685b73c5f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-02 21:28:15 +0100

    end of day commit

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,46 +1,38 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import './index.css';
 //CSS
+import './index.css';
+
+//setup vars
+const firstBook = {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg",
+    author: "Robert T. Kiyosaki",
+    title: "Rich Dad Poor Dad: What the Rich Teach Their…"
+}
+const secondBook = {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg",
+    author: "Dr. Seuss",
+    title: "The Cat in the Hat"
+}
 
  function BookList() {
      return (
          <section className="booklist">
-             <Book />
-             <Book />
-             <Book />
-             <Book />
-             <Book />
-             <Book />
-             <Book />
-             <Book />
-             <Book />
+             <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+             <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
          </section>
      );
  }
 
-const Book = () => {
+const Book = (props) => {
     return (
         <article className="book">
-            <Image />
-            <Title />
-            <Author />
+            <img src={ props.img } alt="" />
+            <h1>{ props.title }</h1>
+            <h4>{ props.author }</h4>
         </article>
     );
 };
- 
-const Image = () => {
-    return <img src="https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg" alt="" />;
-};
-
-const Title = () => {
-    return <h1>Rich Dad Poor Dad: What the Rich Teach Their…</h1>;
-}
-
-const Author = () => {
-    return <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem"}}>Robert T. Kiyosaki</h4>
-}
-
 
 ReactDom.render(<BookList />, document.getElementById('root'));
