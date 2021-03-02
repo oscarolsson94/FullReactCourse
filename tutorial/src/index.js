fcd2cefed50544e77b9b147c9db1dd6059129867
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-02 16:11:50 +0100

    readability and notes

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
-    return <h4>This is Oscar and this is my first component</h4>;
-}
+ function Greeting() {
+     return (
+         <div>
+             <h1>This is Oscar and this is my first component</h1>
+         </div>
+     );
+ }
+
+// const Greeting = () => {
+//     return React.createElement('h1', {}, 'hello world');
+// }
+
+
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
