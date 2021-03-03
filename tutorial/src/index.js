e208c6dd3a869f2cfd67ceca696c9597a5468f8e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-03 11:13:23 +0100

    event handlers

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -46,11 +46,19 @@ const books = [
 
 const Book = ({ img, title, author }) => {
     // const { img, title, author } = props;
+
+    // attribute, eventHandler
+    // onClick, onMouseOver
+    const clickHandler = () => {
+        alert("Hello world")
+    }
+
     return (
         <article className="book">
             <img src={ img } alt="" />
             <h1>{ title }</h1>
             <h4>{author}</h4>
+            <button type="button" onClick={clickHandler}>test</button>
         </article>
     );
 };
