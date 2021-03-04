a17f75b464dfe05803d2275aa399bb38ed8318ce
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-04 17:59:33 +0100

    conditional in useEffect hook

--- advancedReact/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ advancedReact/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -7,7 +7,9 @@ const UseEffectBasics = () => {
     const [value, setValue] = useState(0);
 
     useEffect(() => {
-        document.title = `New Messages(${value})`;
+        if (value > 0) {
+            document.title = `New Messages(${value})`;
+        }      
     });
 
     return <>
