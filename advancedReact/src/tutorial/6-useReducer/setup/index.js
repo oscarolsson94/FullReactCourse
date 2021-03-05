70ce27b8fc7e6d17ada2c1cc078c909e452cc866
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-05 12:05:19 +0100

    useReducer

--- advancedReact/src/tutorial/6-useReducer/setup/index.js
+++ advancedReact/src/tutorial/6-useReducer/setup/index.js
@@ -3,13 +3,44 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 const reducer = (state, action) => {
-    
-};
-
+    if (action.type === "ADD_ITEM") {
+        const newPeople = [...state.people, action.payload];
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: "item added"
+        };
+    }
+    if (action.type === "NO_VALUE") {
+        return {
+            ...state,
+            isModalOpen: true,
+            modalContent: "please enter value"
+        };
+    }
+    if (action.type === "CLOSE_MODAL") {
+        return {
+            ...state,
+            isModalOpen: false,
+            
+        }
+    }
+    if (action.type === "REMOVE_ITEM") {
+        const newPeople = state.people.filter((person) => 
+            person.id !== action.payload
+        )
+        return {
+            ...state,
+            people: newPeople
+        };
+    }
+    throw new Error("no matching action type");
+}
 const defaultState = {
-    people: data,
-    isModalOpen: true,
-    modalContent:"hello world"
+    people: [],
+    isModalOpen: false,
+    modalContent:""
 }
 
 const Index = () => {
@@ -20,29 +51,38 @@ const Index = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         if (name) {
-            
+            const newItem = {
+                id: new Date().getTime().toString(),
+                name
+            };
+            dispatch({ type: "ADD_ITEM", payload: newItem });
         } else {
-            
+            dispatch({ type: "NO_VALUE" });
         }
-    }
+    };
+
+    const closeModal = () => {
+        dispatch({ type: "CLOSE_MODAL" });
+    };
 
     return (
         <>
-            {state.isModalOpen && <Modal modalContent={state.modalContent} />}
-            <form onSubmit={ handleSubmit } className="form">
+            {state.isModalOpen && <Modal closeModal={closeModal} modalContent={state.modalContent} />}
+            <form onSubmit={handleSubmit} className="form">
                 <div>
-                    <input type="text" value={name} onChange={(e)=> setName(e.target.value) }/>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
                 </div>
-            <button type="submit">add</button>
+                <button type="submit">add</button>
             </form>
             {state.people.map((person) => {
-                return <div key={person.id}>
+                return <div key={person.id} className="item">
                     <h4>{person.name}</h4>
+                    <button onClick={() => dispatch({ type: "REMOVE_ITEM", payload: person.id })}>remove</button>
                 </div>
             })}
         </>
-    )
-        ;
+    );
+        
 };
 
 export default Index;
