cc3867222645cf6f7cdface96774d86a0a758f7c
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-08 16:18:41 +0100

    post JSX

--- mern-memories/client/src/components/Posts/Posts.js
+++ mern-memories/client/src/components/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
@@ -9,11 +9,15 @@ const Posts = () => {
     const posts = useSelector((state)=> state.posts); // posts is exported as a combineReducers in reducers/index.js
     const classes = useStyles();
     return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3} >
+                {posts.map((post) => (
+                    <Grid key={post._ID} item item xs={12} sm={6} >
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
