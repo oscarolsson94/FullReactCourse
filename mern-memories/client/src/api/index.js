10de0597385f85f56fd8a7a96272705f592cfc75
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-08 16:37:33 +0100

    axios functions

--- mern-memories/client/src/api/index.js
+++ mern-memories/client/src/api/index.js
@@ -3,5 +3,7 @@ import axios from 'axios'; // to make API calls
 const url = "http://localhost:5000/posts"; //url pointing to backend route
 
 export const fetchPosts = () => axios.get(url);
-
 export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
