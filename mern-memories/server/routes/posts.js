25012b906d505cae2a9272d808b52f74edaf3f7e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-08 10:24:06 +0100

    Getting all the posts in db

--- mern-memories/server/routes/posts.js
+++ mern-memories/server/routes/posts.js
@@ -1,11 +1,14 @@
 import express from 'express';
 
-import {getPosts} from '../controllers/posts.js'
+import {getPosts, createPost} from '../controllers/posts.js'
 
 const router = express.Router();
 
 
 //localhost:5000/posts
 router.get("/", getPosts);
+
+
+router.get("/", createPost);
 
 export default router;
