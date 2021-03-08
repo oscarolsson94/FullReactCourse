b391fc0e4514565346c22c277d9365e62b7aa295
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-08 10:28:41 +0100

    create post route

--- mern-memories/server/controllers/posts.js
+++ mern-memories/server/controllers/posts.js
@@ -9,8 +9,17 @@ export const getPosts = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
 
-export const createPost = (req, res) => {
-    res.send("Post Creation");
-}
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+};
