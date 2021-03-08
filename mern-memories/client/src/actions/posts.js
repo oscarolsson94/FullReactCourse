f7c03030501f38bc96a7566e64595a44d37b8585
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-08 14:34:35 +0100

    form functionality

--- mern-memories/client/src/actions/posts.js
+++ mern-memories/client/src/actions/posts.js
@@ -13,3 +13,17 @@ export const getPosts = () => async (dispatch) => { //redux thunk for async func
 
     
 }
+
+export const createPost = (post) => async (dispatch) => {
+
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: "CREATE", payload: data });
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
--- mern-memories/client/src/api/index.js
+++ mern-memories/client/src/api/index.js
@@ -3,3 +3,5 @@ import axios from 'axios'; // to make API calls
 const url = "http://localhost:5000/posts"; //url pointing to backend route
 
 export const fetchPosts = () => axios.get(url);
+
+export const createPost = (newPost) => axios.post(url, newPost);
