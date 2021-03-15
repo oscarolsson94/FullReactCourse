b7d84490e16b820d05b1ece479d47b46aba9f753
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-03-15 15:19:04 +0100

    taking break

--- redux-testing/src/reducers/counter.js
+++ redux-testing/src/reducers/counter.js
@@ -3,7 +3,9 @@ const counterReducer = (state = 0, action) => {
         case "INCREMENT":
             return state + 1
         case "DECREMENT":
-            return state -1
+            return state - 1
+        default:
+            return state;
     }
 }
 export default counterReducer;
